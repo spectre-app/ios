37d063e1bd8c92a1ea6eb3a7551eb2c4c45b144d
Maarten Billemont <lhunath@lyndir.com>
2020-09-06 01:09:28 -0400

    Assign attributed theme modifiers after assigning attributed string.

--- Sources/View/MPButton.swift
+++ Sources/View/MPButton.swift
@@ -49,6 +49,10 @@ class MPButton: MPEffectView {
         self.action = action
         super.init( border: border, background: background, round: round, rounding: rounding, dims: false )
 
+        self.image = image
+        self.title = title
+        self.attributedTitle = attributedTitle
+
         self.button.titleLabel?.numberOfLines = 0
         self.button.titleLabel?.textAlignment = .center
         self.button.addTarget( self, action: #selector( action(_:) ), for: .primaryActionTriggered )
@@ -57,12 +61,6 @@ class MPButton: MPEffectView {
         self.button.setContentCompressionResistancePriority( .defaultHigh + 1, for: .horizontal )
         self.button.setContentCompressionResistancePriority( .defaultHigh + 1, for: .vertical )
         self.button.setContentHuggingPriority( .defaultHigh, for: .vertical )
-        self.button => \.titleLabel!.font => Theme.current.font.callout
-        //self.button => \.currentAttributedTitle => .font => Theme.current.font.callout
-        self.button => \.currentAttributedTitle => .foregroundColor => Theme.current.color.body
-        self.button => \.currentAttributedTitle => .strokeColor => Theme.current.color.secondary
-        self.button => \.currentTitleColor => Theme.current.color.body
-        self.button.sizeToFit()
 
         self.stateObserver = self.button.observe( \.isSelected, options: .initial ) { [unowned self] _, _ in
             self.isSelected = self.button.isSelected
@@ -76,11 +74,7 @@ class MPButton: MPEffectView {
                 .constrain( margins: true )
                 .activate()
 
-        defer {
-            self.image = image
-            self.title = title
-            self.attributedTitle = attributedTitle
-        }
+        self.update()
     }
 
     override func sizeThatFits(_ size: CGSize) -> CGSize {
@@ -118,6 +112,11 @@ class MPButton: MPEffectView {
         self.button.setImage( self.image, for: .normal )
         self.button.setTitle( self.title, for: .normal )
         self.button.setAttributedTitle( self.attributedTitle, for: .normal )
+        self.button => \.titleLabel!.font => Theme.current.font.callout
+        //self.button => \.currentAttributedTitle => .font => Theme.current.font.callout
+        self.button => \.currentAttributedTitle => .foregroundColor => Theme.current.color.body
+        self.button => \.currentAttributedTitle => .strokeColor => Theme.current.color.secondary
+        self.button => \.currentTitleColor => Theme.current.color.body
         self.button.sizeToFit()
     }
 }
