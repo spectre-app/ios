3aea45e32aefb5bd3ca239f53680c60222e76ff0
Maarten Billemont <lhunath@lyndir.com>
2021-06-09 00:28:34 -0400

    Update source code license terms.
    [UPDATED]   Harmonize copyright statement, creation date, license statement and trademark exclusion.

--- Sources/AutoFill/AutoFillProviderController.swift
+++ Sources/AutoFill/AutoFillProviderController.swift
@@ -1,10 +1,14 @@
+//==============================================================================
+// Created by Maarten Billemont on 2020-09-12.
+// Copyright (c) 2020 Maarten Billemont. All rights reserved.
 //
-//  CredentialProviderViewController.swift
-//  Spectre-AutoFill
+// This file is part of Spectre.
+// Spectre is free software. You can modify it under the terms of
+// the GNU General Public License, either version 3 or any later version.
+// See the LICENSE file for details or consult <http://www.gnu.org/licenses/>.
 //
-//  Created by Maarten Billemont on 2020-09-12.
-//  Copyright © 2020 Lyndir. All rights reserved.
-//
+// Note: this grant does not include any rights for use of Spectre's trademarks.
+//==============================================================================
 
 import AuthenticationServices
 import LocalAuthentication
--- Sources/Util/KeyFactory.swift
+++ Sources/Util/KeyFactory.swift
@@ -1,7 +1,14 @@
-//
+//==============================================================================
 // Created by Maarten Billemont on 2019-10-10.
-// Copyright (c) 2019 Lyndir. All rights reserved.
+// Copyright (c) 2019 Maarten Billemont. All rights reserved.
 //
+// This file is part of Spectre.
+// Spectre is free software. You can modify it under the terms of
+// the GNU General Public License, either version 3 or any later version.
+// See the LICENSE file for details or consult <http://www.gnu.org/licenses/>.
+//
+// Note: this grant does not include any rights for use of Spectre's trademarks.
+//==============================================================================
 
 import UIKit
 import LocalAuthentication
