72040418fe7f2b09bedeb4143b0b1be4e1b66fff
Maarten Billemont <lhunath@lyndir.com>
2021-06-12 22:28:23 -0400

    Reduce gradient.
    [UPDATED]   Minimize usage of visual gradients for a more flat look.

--- Sources/View/BackgroundView.swift
+++ Sources/View/BackgroundView.swift
@@ -101,10 +101,9 @@ class BackgroundView: UIView, ThemeObserver {
         self.imageView.layer.mask = self.imageMask
         self.imageMask.needsDisplayOnBoundsChange = true
         self.imageMask.colors = [
-//            UIColor.black.with( alpha: .long ).cgColor,
-            UIColor.black.with( alpha: .short ).cgColor,
             UIColor.black.with( alpha: .short * .short ).cgColor,
-            UIColor.clear.cgColor
+            UIColor.black.with( alpha: .short * .short ).cgColor,
+            UIColor.black.with( alpha: .off ).cgColor
         ]
         self.imageViewObservation = self.imageView.observe( \.bounds ) { [unowned self] _, _ in
             self.imageMask.frame = self.imageView.bounds
